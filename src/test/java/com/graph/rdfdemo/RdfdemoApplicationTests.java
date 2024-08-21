d53a5f8580959cddec960be5ceb75803f0ece27c
Viswam <visubobby@users.noreply.github.com>
2024-08-21 19:40:24 +0530

    base code

---
+++ src/test/java/com/graph/rdfdemo/RdfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graph.rdfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
